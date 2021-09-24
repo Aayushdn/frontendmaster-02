00b12f90d53ca5b10ef3f8f698b9a0884aef3018
Aayushdn <anyonomousswodari222@gmail.com>
2021-09-24 14:51:56 +0545

    changes favicon and footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navBar = document.querySelector("nav");
+const footDate = document.querySelector(".footDate");
 
 hamburger.addEventListener("click", () => {
   navBar.classList.toggle("open");
   hamburger.classList.toggle("open");
 });
+
+const presentYear = new Date();
+footDate.textContent = presentYear.getUTCFullYear();
